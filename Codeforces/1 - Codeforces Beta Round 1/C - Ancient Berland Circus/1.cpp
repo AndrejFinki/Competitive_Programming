8781d91701d8fd2655a50004a28009325f37c119
A.S. <126242006+AndrejFinki@users.noreply.github.com>
2024-03-25 09:21:02 +0000

    25.03.2024

--- Codeforces/1 - Codeforces Beta Round 1/C - Ancient Berland Circus/1.cpp
+++ Codeforces/1 - Codeforces Beta Round 1/C - Ancient Berland Circus/1.cpp
@@ -21,7 +21,8 @@ public:
 void get_input();
 LD distance( const Point &, const Point & );
 Point midpoint( const Point &, const Point & );
-
+void perpendicular_bisector( const Point &, const Point &, LD &, Point & );
+void find_inner_circle( const Point &, const Point &, const Point &, Point &, LD & );
 
 vector <Point> points;
 
@@ -29,7 +30,19 @@ int main()
 {
 	get_input();
 
+	Point circumcenter;
+	LD circumradius;
+	find_inner_circle( points[0], points[1], points[2], circumcenter, circumradius );
 
+	cout << circumcenter.x << ' ' << circumcenter.y << endl;
+	cout << circumradius << endl;
+
+	/* TODO: 
+	 * Now that we have the circumcenter and circumradius:
+	 * 1 - Transpose all points so that the circumcenter is at (0, 0)
+	 * 2 - Find the angle between points {1, 2}, {1, 3} as a1 and a2
+	 * 3 - Find the first n, such that, m = 360 / n and both a1 and a2 are a multiple of m
+	 */
 }
 
 void get_input()
@@ -44,6 +57,7 @@ LD distance (
 	const Point &p1,
 	const Point &p2
 ) {
+	assert( ( p1.x - p2.x ) * ( p1.x - p2.x ) + ( p1.y - p2.y ) * ( p1.y - p2.y ) >= 0 );
 	return sqrt( 
 		( p1.x - p2.x ) * ( p1.x - p2.x ) +
 		( p1.y - p2.y ) * ( p1.y - p2.y )
@@ -58,4 +72,54 @@ Point midpoint (
 		( p1.x + p2.x ) / 2,
 		( p1.y + p2.y ) / 2
 	);
+}
+
+void perpendicular_bisector(
+	const Point &p1,
+	const Point &p2,
+	LD &slope,
+	Point &mp
+) {
+	mp = midpoint( p1, p2 );
+	if( p1.y == p2.y ) {
+		slope = INFINITY;
+	} else if( p1.x == p2.x ) {
+		slope = 0;
+	} else {
+		assert( p2.x - p1.x );
+		slope = ( p2.y - p1.y ) / ( p2.x - p1.x );
+		assert( slope );
+		slope = -1 / slope;
+	}
+}
+
+void find_inner_circle(
+	const Point &p1,
+	const Point &p2,
+	const Point &p3,
+	Point &center,
+	LD &radius
+) {
+	LD slope1, slope2;
+	Point midpoint1, midpoint2;
+
+	perpendicular_bisector( p1, p2, slope1, midpoint1 );
+	perpendicular_bisector( p2, p3, slope2, midpoint2 );
+
+	if( isinf( slope1 ) && isinf( slope2 ) ) {
+		center.x = midpoint1.x;
+		center.y = ( midpoint1.y + midpoint2.y ) / 2;
+	} else if( !isinf( slope1 ) && isinf( slope2 ) ) {
+		center.x = midpoint2.x;
+		center.y = slope1 * ( center.x - midpoint1.x ) + midpoint1.y;
+	} else if( isinf( slope1 ) && !isinf( slope2 ) ) {
+		center.x = midpoint1.x;
+		center.y = slope2 * ( center.x - midpoint2.x ) + midpoint2.y;
+	} else {
+		assert( slope1 - slope2 );
+		center.x = ( midpoint2.y - midpoint1.y + slope1 * midpoint1.x - slope2 * midpoint2.x ) / ( slope1 - slope2 );
+		center.y = slope1 * ( center.x - midpoint1.x ) + midpoint1.y;
+	}
+
+	radius = distance( center, p1 );
 }
